cfeecccf40d58633f4f7ffafc615bea5703a165a
chun <chy001130@gmail.com>
2020-11-16 09:50:09 -0500

    login form ready

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,43 @@
-$(function(){
-    renderNavBar();
+$(function () {
+    renderLogo();
+    renderLoginForm();
+    //loginOnClick();
+    //homePageContent()
 })
 
+const $root = $('#root');
 
-const renderNavBar = function(){
-    const $root = $('#root');
+const renderLogo = function () {
+    $root.append(`<figure class="image container is-128x128">
+                        <img src="logo.png/"
+                    </figure>`)
+}
 
-    $root.append(`<div class="hero is-fullheight has-background-success-light bg-img">
-                    <div class="hero-body">
-                        <div class="container">
-                            <div class="columns is-centered">
-                                <div class="column is-4-desktop">
+
+const homePageContent = function(){
+    $root.append(`<section class="hero is-medium is-bold">
+<div class="hero-body">
+<div class="container">
+<h1 class="title has-text-white has-text-left">
+Foodies Welcome
+</h1>
+</div>
+</div>
+</section>`)
+}
+const renderLoginForm = function () {
+    //const $root = $('#root');
+
+    $root.append(`<div class="hero">
+                    <div class="hero-body">                   
+                        <div class="container">                      
+                            <div class="columns is-centered">                      
+                                <div class="column is-4-desktop">                            
                                     <form class="box">
                                        <div class="field">
                                            <label class="label">Email</label>
                                         <p class="control has-icons-left has-icons-right">                                        
-                                            <input class="input" type="email" placeholder="Email">
+                                            <input class="input" id="userEmail"  type="email" placeholder="Email">
                                                 <span class="icon is-small is-left">
                                                     <i class="fas fa-at"></i>    
                                                 </span>
@@ -23,38 +45,51 @@ const renderNavBar = function(){
                                                     <i class="fas fa-check"></i>
                                                 </span>
                                         </p>
+                                        </div>
+                                            <div class="field">
+                                            <label class="label">Password</label>
+                                              <p class="control has-icons-left">
+                                                <input class="input" id="userPassword" type="password" placeholder="Password">
+                                                <span class="icon is-small is-left">
+                                                  <i class="fas fa-lock"></i>
+                                                </span>
+                                              </p>
+                                            </div>
+                                        <div class="field">
+                                                <a href="#">Sign up</a>                                                             
+                                        </div>
+                                        <div class="field">
+                                                <a href="#">Forgot Password?</a>                                                             
+                                        </div>                                                                      
+                                        <div class="field">
+                                          <p class="control">
+                                            <button class="button is-rounded is-success loginButton">
+                                              Login
+                                            </button>
+                                          </p>
+                                        </div>
+                                    
+                                    </form>
                                 </div>
-                                    <div class="field">
-                                    <label class="label">Password</label>
-                                      <p class="control has-icons-left">
-                                        <input class="input" type="password" placeholder="Password">
-                                        <span class="icon is-small is-left">
-                                          <i class="fas fa-lock"></i>
-                                        </span>
-                                      </p>
-                                    </div>
-                                    <div class="field">
-                                    <a href="#">Sign up</a>                                                             
-                                    </div>
-                                    <div class="field">
-                                    <a href="#">Forgot Password?</a>                                                             
-                                    </div>
-                                    
-                                    
-                                    <div class="field">
-                                      <p class="control">
-                                        <button class="button is-rounded is-success">
-                                          Login
-                                        </button>
-                                      </p>
-                                    </div>
-                                    
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>     `)
+                            </div>
+                        </div>
+                    </div>
+                </div>`)
 
 
+}
+
+const renderSignUpModal = function(){
+
+}
+
+const loginOnClick = function () {
+    //const $root = $('#root');
+
+    let userEmail = ''
+    let userPassword = ''
+    $root.on('click', '.loginButton', function () {
+        userEmail = $("input#userEmail").val()
+        userPassword = $("input#userPassword").val()
+    })
 }
